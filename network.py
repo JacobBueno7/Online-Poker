2cf0f88d862f148cdad89b981b3d4a0e331e84d0
Jacob <buenojacob743@gmail.com>
2023-12-30 17:01:01 -0800

    deleted test file

--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 import socket
 class Network:
     def __init__(self):
-        self.client =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = "192.168.0.40"
         self.port = 5555
         self.addr = (self.host, self.port)
